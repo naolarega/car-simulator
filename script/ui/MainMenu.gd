138bd931219b0407c6ae127e32b0ad461d39a3b6
naolArega <na408498@gmail.com>
2021-11-13 20:11:07 +0300

    added logging singleton added main map added logo

--- script/ui/MainMenu.gd
+++ script/ui/MainMenu.gd
@@ -1,2 +1,4 @@
 extends Control
 
+func _ready():
+	OS.print_all_resources()
--- script/ui/Splash.gd
+++ script/ui/Splash.gd
@@ -3,4 +3,5 @@ extends Control
 export var menu_scene : PackedScene
 
 func open_menu():
-	get_tree().change_scene_to(menu_scene)
+	if get_tree().change_scene_to(menu_scene) != OK:
+		printerr('couldn\'t open main menu!')
